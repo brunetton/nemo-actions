2b491a319220240f9335349095bdb604ed706956
Bruno Duyé <brunetton@gmail.com>
2018-01-25 00:04:18 +0100

    Add stabilization factor select dialog + (quick & dirty) translations

--- scripts/stab_vids.rb
+++ scripts/stab_vids.rb
@@ -9,11 +9,30 @@ require 'open3'
 require 'fileutils'
 
 
-ZENITY_WITH_OPTIONS = 'zenity --progress --title=Working... --text=Stabilisation... --auto-close'
-SMOOTHING_FACTOR = 6  # Number of frames to look ahead and forward. See https://github.com/georgmartius/vid.stab#second-pass-vidstabtransform-filter
+LANG = :en  # :en or :fr
+
+
+TEXTS = {
+  stabilization: {
+    en: 'Stabilization ...',
+    fr: 'Stabilisation ...',
+  },
+  smooth_factor: {
+    en: 'Select smoothing factor\n(number of frames before <b>and after</b> where to look to detect movement)',
+    fr: "Sélectionnez le smoothing factor\n(nombre d'images avant <b>et après</b> l'image actuelle\noù détecter du mouvement)",
+  },
+  finished: {
+    en: 'Finished !',
+    fr: 'Fini !',
+  },
+}
+
+
+ZENITY_WITH_OPTIONS = "zenity --progress --title=Working... --text=#{TEXTS[:stabilization][LANG]} --auto-close"
+DEFAULT_SMOOTHING_FACTOR = 6  # Number of frames to look befre and after current frame to detect movement.
 FFMPEG_COMMANDS = [
   'ffmpeg -loglevel error -i "{input_file}" -vf vidstabdetect=result="{input_file}.trf" -f null -',
-  'ffmpeg -loglevel error -i "{input_file}" -vf vidstabtransform=input="{input_file}.trf":smoothing={smoothing} -acodec copy -vcodec h264 -preset slow -tune film -crf 23 "{output_file}"'
+  'ffmpeg -loglevel error -i "{input_file}" -vf vidstabtransform=input="{input_file}.trf":smoothing={smoothing_factor} -acodec copy -vcodec h264 -preset slow -tune film -crf 23 "{output_file}"'
 ]
 
 $log = ''  # Unused for now, but could be presented to user at the end of the process
@@ -32,11 +51,11 @@ def exec_command(command)
   return log
 end
 
-def ffmpeg_pass(input_filename, pass_nb, out_dir)
+def ffmpeg_pass(input_filename, pass_nb, smoothing_factor, out_dir)
   trf_file = "#{input_filename}.trf"
   File.delete(trf_file) if pass_nb == 1 and File.exist?(trf_file)
   output_file = File.join(out_dir, File.basename(input_filename))
-  command = FFMPEG_COMMANDS[pass_nb-1].gsub('{input_file}', input_filename).gsub('{output_file}', output_file).gsub('{smoothing}', String(SMOOTHING_FACTOR))
+  command = FFMPEG_COMMANDS[pass_nb-1].gsub('{input_file}', input_filename).gsub('{output_file}', output_file).gsub('{smoothing_factor}', smoothing_factor)
   $log += exec_command(command)
 end
 
@@ -67,6 +86,14 @@ out_dir = File.join(File.join(source_dir, "stab"))
 FileUtils.rm_r(out_dir) if Dir.exist?(out_dir)
 Dir.mkdir(out_dir)
 
+# Ask user for smoothing factor
+# See https://github.com/georgmartius/vid.stab
+smoothing_factor = `zenity --scale --text="#{TEXTS[:smooth_factor][LANG]}" --value=#{DEFAULT_SMOOTHING_FACTOR} --min-value=2 --max-value=30 --step 4`
+if not $?.success?
+  puts "Cancelled by user"
+  exit
+end
+
 total_nb_files = files_to_treat.length
 total_nb_actions = total_nb_files * 2
 
@@ -76,11 +103,11 @@ begin
     files_to_treat.each do |filename|
       puts("=> " + filename)
       # First pass
-      ffmpeg_pass(filename, 1, out_dir)
+      ffmpeg_pass(filename, 1, smoothing_factor, out_dir)
       actions += 1
       io.puts(percent(actions, total_nb_actions))  # send percent to zenity
       # Second pass
-      ffmpeg_pass(filename, 2, out_dir)
+      ffmpeg_pass(filename, 2, smoothing_factor, out_dir)
       actions += 1
       io.puts(percent(actions, total_nb_actions))  # send percent to zenity
     end
@@ -94,4 +121,4 @@ Dir[File.join(source_dir, '*.trf')].each{ |filename| File.delete(filename) }
 puts "\nEND"
 
 # End
-%x( zenity --info --text="Fini !" )
+%x( zenity --info --text="#{TEXTS[:finished][LANG]}" )
