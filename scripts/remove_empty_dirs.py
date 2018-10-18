a12cebab3b7d778e9b0671e9cf7669f99683dcae
Bruno <brunetton@gmail.com>
2018-10-18 17:02:20 +0200

    remove_empty_dirs: fix some missing empty dirs were present

--- scripts/remove_empty_dirs.py
+++ scripts/remove_empty_dirs.py
@@ -4,6 +4,9 @@
 import os
 import sys
 
-for dirpath, dirnames, filenames in os.walk(sys.argv[1], topdown=True, onerror=None, followlinks=False):
-    if len(filenames) + len(dirnames) == 0:
+for dirpath, dirnames, filenames in os.walk(sys.argv[1], topdown=False, onerror=None, followlinks=False):
+    print("dirpath: {!r}, dirnames: {!r}, filenames: {!r}".format(dirpath, dirnames, filenames))
+    # We don't use dirnames given by os.walk() as it's not updated with deleted ones
+    actual_dirnames = [dirname for dirname in os.listdir(dirpath) if os.path.isdir(os.path.join(dirpath, dirname))]  # List source_dir subdirs
+    if len(filenames) + len(actual_dirnames) == 0:
         os.rmdir(dirpath)
