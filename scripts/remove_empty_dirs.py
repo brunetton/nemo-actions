0a5ac7bbaa10a11980a7ca2af171b8869d28655e
Bruno Duyé <brunetton@brunetton.org>
2023-09-03 23:11:05 +0200

    Minor: remove_empty_dirs.py, remove debug messages

--- scripts/remove_empty_dirs.py
+++ scripts/remove_empty_dirs.py
@@ -8,7 +8,7 @@ if len(sys.argv) != 2:
     print("Syntax: remove_empty_dirs [path]")
 
 for dirpath, dirnames, filenames in os.walk(sys.argv[1], topdown=False, onerror=None, followlinks=False):
-    print("dirpath: {!r}, dirnames: {!r}, filenames: {!r}".format(dirpath, dirnames, filenames))
+    # print("dirpath: {!r}, dirnames: {!r}, filenames: {!r}".format(dirpath, dirnames, filenames))
     # We don't use dirnames given by os.walk() as it's not updated with deleted ones
     actual_dirnames = [dirname for dirname in os.listdir(dirpath) if os.path.isdir(os.path.join(dirpath, dirname))]  # List source_dir subdirs
     if len(filenames) + len(actual_dirnames) == 0:
